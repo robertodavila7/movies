64f767c4202183e623cc76f53129936cab3a5967
robertodavila7 <50179736+robertodavila7@users.noreply.github.com>
2022-07-18 20:10:27 -0500

    Updated CSS

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.REACT_APP_PUBLIC_UR}>
+    <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
